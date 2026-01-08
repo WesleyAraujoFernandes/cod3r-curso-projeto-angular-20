82e5785f2bafde6c61beeabd546f1f4092a5f8a6
WesleyAraujoFernandes <wesley.a.fernandes@gmail.com>
2026-01-08 15:53:37 -0300

    criação das rotas de counter e todos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,23 @@
 import { Routes } from '@angular/router';
 import { Home } from './screens/home/home';
+import { Counter } from './screens/counter/counter';
+import { Todos } from './screens/todos/todos';
 
 export const routes: Routes = [
   {
     path: '',
     component: Home
+  },
+  {
+    path: 'counter',
+    loadComponent() {
+      return import('./screens/counter/counter').then((m) => m.Counter);
+    }
+  },
+  {
+    path: 'todos',
+    loadComponent() {
+      return import('./screens/todos/todos').then((m) => Todos);
+    }
   }
 ];
