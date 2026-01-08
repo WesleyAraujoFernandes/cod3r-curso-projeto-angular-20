12ab4cf87f56cc448e9d3daab97654716ff20fd6
WesleyAraujoFernandes <wesley.a.fernandes@gmail.com>
2026-01-08 17:41:35 -0300

    criação dos indicativos, na tela, de itens concluídos ou pendentes

--- src/app/services/todos.service.ts
+++ src/app/services/todos.service.ts
@@ -7,7 +7,7 @@ import { Todo } from '../model/todo.entity';
 export class TodosService {
   private readonly _items = signal<Array<Todo>>([
     { id: crypto.randomUUID(), title: 'Implementar App Mobile', completed: false },
-    { id: crypto.randomUUID(), title: 'Estudar Angular Avançado', completed: false },
+    { id: crypto.randomUUID(), title: 'Estudar Angular Avançado', completed: true },
     { id: crypto.randomUUID(), title: 'Revisar Código do Projeto', completed: false },
   ]);
   readonly items = this._items.asReadonly();
